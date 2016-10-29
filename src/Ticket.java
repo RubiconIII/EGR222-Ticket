da567b7bf3a7566c9e5a34da0d8feffdecd4cea7
Curtis Hohl <curtispatrick.hohl@calbaptist.edu>
2016-10-28 18:22:38 -0700

    Ticket program

---
+++ src/Ticket.java
@@ -0,0 +1,30 @@
+/**
+ * Created by curti_000 on 10/26/2016.
+ */
+public class Ticket{
+    protected int number;
+    protected double price;
+
+    //default constructor
+    public Ticket(int number) {
+        this.number = number;
+        this.price = 50;
+    }
+
+    //second constructor
+    public Ticket(int number, double price) {
+        this.number = number;
+        this.price = price;
+    }
+
+    //default getPrice()
+    public double getPrice() {
+       return price;
+    }
+
+    //default toString()
+    public String toString() {
+        return "Number: " + number  + ", Price: " + price;
+    }
+}
+
